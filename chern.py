ea9cedee31abb9aa0688fdf5a1aaf0a7c23a8edc
shadow22852 <misterio.clok@gmail.com>
2024-05-30 03:49:56 +0300

    modified:   api.py 	new file:   chern.py 	modified:   test.py

---
+++ chern.py
@@ -0,0 +1,32 @@
+import json
+with open('keys.json') as f:
+    data = json.load(f)
+    keys = list(data.keys())
+
+# for ke in keys:
+#     if "bearer" in ke:
+#         print(f"{ke}:{data[ke]}")
+        
+# mass = []
+
+# for ke in keys:
+#     if "bearer" in ke:
+#         mass.append(data[ke])
+
+# print(mass)
+
+def get_bearer_from_json():
+    bearer_list = []
+    with open ("keys.json") as file:
+        data = json.load(file)
+        keys = list(data.keys())
+    for k in keys:
+        if "bearer" in k:
+            bearer_list.append(data[k])
+    return bearer_list
+
+bearer_list = get_bearer_from_json()
+print(len(bearer_list))
+for x in range(len(bearer_list)):
+    bearer_token = str(bearer_list[x])
+    print(bearer_token)
--- test.py
+++ test.py
@@ -1,46 +1,70 @@
 import tweepy
 import pandas as pd
 import datetime
+import os
+import json
 
-consumer_key = "fSAXyRrt7GITUSSo4IlqhgYtI" #Your API/Consumer key 
-consumer_secret = "iKshVuyGrBmlSNPkwFP0Svnhowbj9YE1NUOKkSm6sj1gyxJyv9" #Your API/Consumer Secret Key
-access_token = "806765394199330817-yXWqaTwmBhBdUJvjgOvg0h9UsM9HG5D"    #Your Access token key
-access_token_secret = "uw0pzAbA5vujhOdatP2aWklpakM5bDEQqqDsQUa2aQPy6" #Your Access token Secret key
+with open ("keys.json") as f:
+    data = json.load(f)
+    keys = list(data.keys())
 
+os.system("cls")
+def get_bearer_from_json():
+    bearer_list = []
+    with open ("keys.json") as file:
+        data = json.load(file)
+        keys = list(data.keys())
+    for k in keys:
+        if "bearer" in k:
+            bearer_list.append(data[k])
+    return bearer_list
 
-# Аутентификация
-auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
+bearer_list = get_bearer_from_json()
 
-# Функция для получения твитов
-def get_tweets(username, since_date):
-    tweets_data = []
-    for tweet in tweepy.Cursor(api.user_timeline, screen_name=username, tweet_mode="extended").items():
-        if tweet.created_at >= since_date:
+for x in len(bearer_list):
+    bearer_token = str(bearer_list[x])
+
+    # Аутентификация
+    client = tweepy.Client(bearer_token=bearer_token)
+
+    # Функция для получения твитов
+    def get_tweets(username, since_date):
+        tweets_data = []
+        user = client.get_user(username=username)
+        user_id = user.data.id
+
+        query = f'from:{username} -is:retweet'
+        end_time = since_date.isoformat("T") + "Z"
+        
+        paginator = tweepy.Paginator(client.search_all_tweets, query=query, tweet_fields=['created_at', 'public_metrics'], 
+                                    start_time=end_time, max_results=100)
+        
+        for tweet in paginator.flatten(limit=1000):  # ограничение по количеству твитов
             tweet_info = {
                 'id': tweet.id,
                 'created_at': tweet.created_at,
-                'full_text': tweet.full_text,
-                'retweet_count': tweet.retweet_count,
-                'favorite_count': tweet.favorite_count,
-                'reply_count': tweet.reply_count if hasattr(tweet, 'reply_count') else None,
-                'quote_count': tweet.quote_count if hasattr(tweet, 'quote_count') else None
+                'full_text': tweet.text,
+                'retweet_count': tweet.public_metrics['retweet_count'],
+                'reply_count': tweet.public_metrics['reply_count'],
+                'like_count': tweet.public_metrics['like_count'],
+                'quote_count': tweet.public_metrics['quote_count']
             }
             tweets_data.append(tweet_info)
-        else:
-            break
-    return tweets_data
+        
+        return tweets_data
 
-# Основной код
-username = 'POTUS'  # Официальный аккаунт Джо Байдена
-since_date = datetime.datetime.now() - datetime.timedelta(days=365)  # За последний год
+    # Основной код
+    username = 'POTUS'  # Официальный аккаунт Джо Байдена
+    since_date = datetime.datetime.now() - datetime.timedelta(days=365)  # За последний год
 
-tweets_data = get_tweets(username, since_date)
+    tweets_data = get_tweets(username, since_date)
 
-# Создание DataFrame
-df = pd.DataFrame(tweets_data)
-print(df)
-# Сохранение в CSV файл
-df.to_csv(f'{username}_tweets_stats.csv', index=False)
+    # Создание DataFrame
+    df = pd.DataFrame(tweets_data)
+    print(df)
+    # Сохранение в CSV файл
+    df.to_csv(f'{username}_tweets_stats.csv', index=False)
 
-print(f"Статистика твитов сохранена в {username}_tweets_stats.csv")
+    print(f"Статистика твитов сохранена в {username}_tweets_stats.csv")
+
+
